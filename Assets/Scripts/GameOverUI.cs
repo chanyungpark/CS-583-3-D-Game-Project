7568ec428eee9f38b520fa4eee403b1a931e2ae9
chanyungpark <222356440+chanyungpark@users.noreply.github.com>
2025-12-17 14:21:40 -0800

    Add GameOverUI script

---
+++ Assets/Scripts/GameOverUI.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class GameOverUI : MonoBehaviour
+{
+    public static GameOverUI Instance;
+
+    public GameObject root;
+    public TMP_Text reasonText;
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        root.SetActive(false);
+    }
+
+    public void ShowGameOver(string reason)
+    {
+        root.SetActive(true);
+        reasonText.text = reason;
+
+        Time.timeScale = 0f;
+
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+
+    public void Restart()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void GoToMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
