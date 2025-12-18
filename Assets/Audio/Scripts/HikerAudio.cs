8f24e66c89c06949049160d3a61e888294211306
ryanrenales <161529467+ryanrenales@users.noreply.github.com>
2025-12-17 17:16:04 -0800

    Hiker Panic Audio added

--- Assets/Audio/Scripts/HikerAudio.cs
+++ Assets/Audio/Scripts/HikerAudio.cs
@@ -1,15 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Collections;
 
 public class HikerAudio : MonoBehaviour
 {
-    [Header("Audio Sources")]
     private AudioSource source;
+    private Coroutine panicRoutine;
 
     [Header("Clips")]
-    public AudioClip panicLoop;
+    public AudioClip panicClip;
     public AudioClip rescuedClip;
+
+    [Header("Timing")]
+    public float panicInterval = 8f;
 
     private bool isPanicking = false;
 
@@ -18,14 +22,17 @@ public class HikerAudio : MonoBehaviour
         source = GetComponent<AudioSource>();
     }
 
+    private void Start()
+    {
+        StartPanic();
+    }
+
     public void StartPanic()
     {
-        if (isPanicking) return;
+        if (isPanicking || panicClip == null) return;
 
         isPanicking = true;
-        source.clip = panicLoop;
-        source.loop = true;
-        source.Play();
+        panicRoutine = StartCoroutine(PanicLoop());
     }
 
     public void StopPanic()
@@ -33,14 +40,33 @@ public class HikerAudio : MonoBehaviour
         if (!isPanicking) return;
 
         isPanicking = false;
+
+        if (panicRoutine != null)
+        {
+            StopCoroutine(panicRoutine);
+            panicRoutine = null;
+        }
+
         source.Stop();
-        source.loop = false;
+    }
+
+    private IEnumerator PanicLoop()
+    {
+        while (isPanicking)
+        {
+            source.PlayOneShot(panicClip);
+            yield return new WaitForSeconds(panicInterval);
+        }
     }
 
     public void PlayRescued()
     {
         StopPanic();
-        source.PlayOneShot(rescuedClip);
+
+        if (rescuedClip != null)
+        {
+            source.PlayOneShot(rescuedClip);
+        }
     }
 }
 
